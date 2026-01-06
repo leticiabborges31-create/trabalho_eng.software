2cf7d21afbe9d2c02b1d4e25db07876d39ed5283
Barbs Ellen <91839395+BarbsII@users.noreply.github.com>
2026-01-06 15:31:39 -0300

    Add Classe Tarefa

---
+++ src/Tarefa.py
@@ -0,0 +1,25 @@
+class Tarefa:
+    nome = ""
+    descricao = ""
+    status = ""
+    
+    def __init__(self, nome, descricao):
+        self.nome = nome
+        self.descricao = descricao
+        pass
+
+    def get_status():
+        pass
+    def set_status():
+        pass
+
+    def get_nome():
+        pass
+    def set_nome():
+        pass
+
+    def get_descricao():
+        pass
+    def set_descricao():
+        pass
+    
